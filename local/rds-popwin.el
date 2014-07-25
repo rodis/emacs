c95825aadd55fa65b91cf2d22695971e0b82b324
Rosario Di Somma <rosario.disomma@dreamhost.com>
2014-07-25 03:32:17 +0200

    Remove helm's buffers from popwin list
    Change-Id: I369ec711265fc9a71b2af2c5a56d5214f92c5c01
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- local/rds-popwin.el
+++ local/rds-popwin.el
@@ -1,14 +1,16 @@
+;;; package --- popwin configuration
+
+;;; Commentary:
 (require 'popwin)
 
+;;; Code:
 (setq display-buffer-function 'popwin:display-buffer)
-(setq popwin:special-display-config 
+(setq popwin:special-display-config
       '(("*Help*" :height 30)
         ("*Completions*" :noselect t)
         ("*Occur*" :stick t)
         ("*Browse code*")
 ))
 
-(setq helm-full-frame nil)
-(push '("^\*helm .+\*$" :regexp t) popwin:special-display-config)
-(push '("^\*helm-.+\*$" :regexp t) popwin:special-display-config)
 (provide 'rds-popwin)
+;;; rds-popwin ends here
