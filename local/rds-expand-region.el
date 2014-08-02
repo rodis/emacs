504723c8087dbf49b659d527e897b0689366700e
Rosario Di Somma <rosario.disomma@dreamhost.com>
2014-08-02 17:58:58 +0200

    Move expand-region conf to the el-get file
    Change-Id: I7403936f536e7dd7d9db30b0f8703982e62e6c27
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- local/rds-expand-region.el
+++
@@ -1,8 +0,0 @@
-(require 'expand-region)
-
-(global-set-key [?\M-9] 'er/expand-region)
-
-(setq expand-region-contract-fast-key "0")
-(setq expand-region-reset-fast-key "-")
-
-(provide 'rds-expand-region)
