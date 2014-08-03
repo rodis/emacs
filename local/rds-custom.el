79a4a4de4b6d34e139be66696f25edbe3d76b616
Rosario Di Somma <rosario.disomma@dreamhost.com>
2014-08-03 02:59:03 +0200

    Cleanup custom file
    Change-Id: Ibc36ec2296da0713d1d010af807dafeb7223deb9
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- local/rds-custom.el
+++ local/rds-custom.el
@@ -1,12 +1,14 @@
+;;; custome --- Custom variables
+
+;;; Commentary:
+
+;;; Code:
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(protect-buffer-bury-p nil t)
- '(safe-local-variable-values (quote ((encoding . utf-8))))
- '(show-paren-mode t)
  '(transient-mark-mode t))
 
 (custom-set-faces
@@ -14,16 +16,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(diredp-dir-heading ((t (:foreground "#FFA927"))))
- '(diredp-exec-priv ((t (:foreground "LightStee	lBlue"))))
- '(diredp-file-name ((t nil)))
- '(diredp-no-priv ((t (:foreground "Black"))))
- '(diredp-number ((t (:foreground "#FFA927"))))
- '(diredp-read-priv ((t (:foreground "#5fd7af"))))
- '(diredp-write-priv ((t (:foreground "Orchid"))))
  '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-warning :background "red" :foreground "#ffffff" :weight normal))))
  '(flycheck-color-mode-line-warning-face ((t (:inherit flycheck-fringe-warning :background "#859900" :foreground "#ffffff" :weight normal))))
+ '(helm-selection ((t (:background "#303030" :underline nil))))
+ '(helm-source-header ((t (:background "#1B1E1C" :foreground "#FFFFFF" :underline nil :weight bold))))
  '(persp-selected-face ((t (:foreground "#5FFFFF" :weight bold))))
- '(powerline-active1 ((t (:inherit mode-line :background "grey22" :foreground "white"))))
- '(powerline-active2 ((t (:inherit mode-line :background "grey40" :foreground "white"))))
- '(vertical-border ((t (:background "black" :foreground "black")))))
+)
+
+;;; rds-custom.el ends here
