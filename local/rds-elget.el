74529e90d4304675f4c2cb0cc5ae92f949f7e7af
Rosario Di Somma <rosario.disomma@dreamhost.com>
2014-08-03 02:52:17 +0200

    Clean up the elget file
    Change-Id: I1a2a118c122e1796e5d98ea2b355c820b5fb6b2f
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- local/rds-elget.el
+++ local/rds-elget.el
@@ -17,35 +17,27 @@
 (setq el-get-sources '(
 
 	(:name backup-each-save
-           :description "Backup each savepoint of a file"
            :url "http://www.emacswiki.org/emacs/download/backup-each-save.el"
            :type emacswiki)
 
 	(:name popup
-           :website "https://github.com/auto-complete/popup-el"
-           :description "Visual Popup Interface Library for Emacs"
            :type github
            :pkgname "auto-complete/popup-el"
            :submodule nil
            :shallow t)
 
 	(:name auto-complete
-           :website "https://github.com/auto-complete/auto-complete"
-           :description "The most intelligent auto-completion extension."
            :type github
            :pkgname "auto-complete/auto-complete"
            :submodule nil
            :shallow t)
 
 	(:name fuzzy
-           :website "https://github.com/auto-complete/fuzzy-el"
-           :description "Fuzzy matching utilities for GNU Emacs"
            :type github
            :pkgname "auto-complete/fuzzy-el"
            :shallow t)
 
 	(:name powerline
-           :description "Emacs version of the Vim powerline."
            :type github
            :pkgname "milkypostman/powerline"
            :compile nil
@@ -53,26 +45,15 @@
            :shallow t)
 
 	(:name flycheck-color-mode-line
-           :description "An Emacs minor-mode for Flycheck which colors the mode line according to the Flycheck state of the current buffer."
            :type github
            :pkgname "syl20bnr/flycheck-color-mode-line"
            :shallow t)
 
-	;; (:name emacs-color-theme
-    ;;        :description "Emacs color themes inspired by some ST2 color themes"
-    ;;        :type github
-    ;;        :pkgname "rodis/emacs-color-themes"
-    ;;        :shallow t
-    ;;        :post-init (lambda ()
-    ;;                     (add-to-list 'custom-theme-load-path "~/.emacs.d/el-get/emacs-color-theme/themes")))
-
 	(:name python24
-	       :description "Python's flying circus support for Emacs (24.x)"
 	       :type http
 	       :url "http://repo.or.cz/w/emacs.git/blob_plain/refs/heads/emacs-24:/lisp/progmodes/python.el")
 
     (:name helm
-           :description "Emacs incremental and narrowing framework"
            :type github
            :pkgname "emacs-helm/helm"
            :compile nil
@@ -80,7 +61,6 @@
            :shallow t)
 
     (:name ergoemacs-mode
-           :description ""
            :type github
            :pkgname "ergoemacs/ergoemacs-mode"
            :shallow t)
@@ -88,19 +68,16 @@
     (:name flycheck
            :type github
            :pkgname "lunaryorn/flycheck"
-           :description "On-the-fly syntax checking extension"
            :depends (s dash cl-lib f pkg-info)
            :shallow t)
 
 	(:name win-switch
-           :description "Windows navigation library"
            :url "http://www.emacswiki.org/emacs/download/win-switch.el"
            :type emacswiki)
 
     (:name monokai
            :type github
            :pkgname "oneKelvinSmith/monokai-emacs"
-           :description "Monokai is a port of the popular TextMate theme Monokai"
            :shallow t
            :before (progn
                      (add-to-list 'custom-theme-load-path
@@ -113,8 +90,6 @@
                     (global-set-key [?\M-9] 'er/expand-region)
                     (setq expand-region-contract-fast-key "0")
                     (setq expand-region-reset-fast-key "-")))
-;;     (:name perspective
-;;            :after (progn (persp-mode)))
 ))
 
 (setq my-packages
